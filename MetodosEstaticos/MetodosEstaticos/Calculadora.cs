714f544cd414218b5b23cf386a0febeea1988004
janRocha <jan.jrs@hotmail.com>
2021-03-10 19:32:45 -0300

    Aula 46 - Sentando o nome static dessa forma não precisa estanciar a classe versão 3

--- MetodosEstaticos/MetodosEstaticos/Calculadora.cs
+++ MetodosEstaticos/MetodosEstaticos/Calculadora.cs
@@ -2,12 +2,12 @@
 {
     class Calculadora
     {
-        public double pi = 3.14;
-        public double Circunferencia(double raio)
+        public static double pi = 3.14;
+        public static double Circunferencia(double raio)
         {
             return 2.0 * pi * raio;
         }
-        public double volume(double raio)
+        public static double volume(double raio)
         {
             return 4.0 / 3.0 * pi * raio * raio * raio;
         }
--- MetodosEstaticos/MetodosEstaticos/Program.cs
+++ MetodosEstaticos/MetodosEstaticos/Program.cs
@@ -9,14 +9,13 @@ namespace MetodosEstaticos
    
         static void Main(string[] args)
         {
-            Calculadora Calc = new Calculadora();
             Console.Write("Entre o valor do raio");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double circ = Calc.Circunferencia(raio);
-            double vol = Calc.volume(raio);
+            double circ = Calculadora.Circunferencia(raio);
+            double vol = Calculadora.volume(raio);
             Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("vol: " + vol.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Pi:  " + Calc.pi.ToString());
+            Console.WriteLine("Pi:  " + Calculadora.pi.ToString());
         }
         
     }
