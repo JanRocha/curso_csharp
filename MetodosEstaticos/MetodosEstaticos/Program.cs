b268011c819c592a5c74b88926ad512fb5ab3279
janRocha <jan.jrs@hotmail.com>
2021-03-10 14:54:50 -0300

    Aula 46 - Implementação da versão 1 do metodo statico

---
+++ MetodosEstaticos/MetodosEstaticos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace MetodosEstaticos
+{
+    class Program
+    {
+        static double pi = 3.14;
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o valor do raio");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double circ = Circunferencia(raio);
+            double vol = volume(raio);
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("vol: " + vol.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Pi:  " + pi.ToString());
+        }
+        static double Circunferencia(double raio)
+        {
+            return 2.0 * pi * raio;
+        }
+        static double volume (double raio)
+        {
+            return 4.0 / 3.0 * pi * raio * raio * raio;
+        }
+    }
+}
