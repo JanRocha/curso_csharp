63880a105c413742b8a554a16c5dc428dc2d6f9e
janRocha <jan.jrs@hotmail.com>
2021-03-10 22:35:14 -0300

    Aula 47 - Exercicio de fixação. calculadora de compra de dolar.

---
+++ ConvesorDeMoedas/Conversor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvesorDeMoedas
+{
+    class Conversor
+    {
+        public static double iof = 6;
+
+        public static double Converter(double cotacao, double valor)
+        {
+            double calc = valor * cotacao;
+            calc = calc + ((calc * iof) / 100);
+            return calc;
+        }
+    }
+}
---
+++ ConvesorDeMoedas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConvesorDeMoedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a contação di dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Quantos dolares desaja comprar? ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double total = Conversor.Converter(cotacao, dolar);
+            Console.Write("valor a ser pago em reais : " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
